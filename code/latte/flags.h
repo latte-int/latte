a0f64fb544f2c6c7dd43aceb95ec2c5bb9866c31
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-02-24 00:29:46 +0000

    (struct BarvinokParameters): New.

--- code/latte/flags.h
+++ code/latte/flags.h
@@ -1,3 +1,4 @@
+/* This is a -*- C++ -*- header file. */
 #ifndef FLAGS_H
 #define FLAGS_H 1
 
@@ -9,5 +10,16 @@
 #define DECOMPOSE 0x10
 #define LOAD	0x20
 #define SAVE    0x40
+
+struct BarvinokParameters {
+  // Whether we use the
+  //   - traditional LattE monomial substitution z_i |-> (1 + s)^(lambda_i) 
+  //   - or the exponential substitution         z_i |-> exp(t lambda_i)
+  enum { PolynomialSubstitution, ExponentialSubstitution } substitution;
+  // The maximum determinant of cones that we do not subdivide
+  // further.  Set to 1 to subdivide until we reach unimodular cones
+  // only.
+  int max_determinant;
+};
 
 #endif
