62a9cd308a6892e6170315588626e64180c39108
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-02-26 19:49:18 +0000

    Compare Taylor'ed Todd with Maple'd Todd.

--- code/latte/todd/test-todd.cpp
+++ code/latte/todd/test-todd.cpp
@@ -2,25 +2,32 @@
 #include <cstdlib>
 
 #include "todd/todd.h"
+#include "todd/todd-expansion.h"
 
 using namespace std;
 
 void usage()
 {
-  cerr << "usage: test-todd DIMENSION K X_1 ... X_DIMENSION" << endl;
+  cerr << "usage: test-todd X_1 ... X_d" << endl;
   exit(1);
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 3) usage();
-  int dim = atoi(argv[1]);
-  int k = atoi(argv[2]);
-  if (argc != dim + 3) usage();
-  mpz_class *x = new mpz_class[dim];
+  if (argc < 2) usage();
+  int dim = argc - 1;
+  vector<mpz_class> x(dim);
   int i;
   for (i = 0; i<dim; i++) {
-    x[i] = mpz_class(atoi(argv[3 + i]));
+    x[i] = mpz_class(atoi(argv[1 + i]));
   }
-  cout << todd(dim, k, x) << endl;
+  int k;
+  for (k = 0; k<=dim; k++)
+    cout << todd(dim, k, x) << " ";
+  cout << endl;
+  vector<mpq_class> todds = evaluate_todd(x);
+  vector<mpq_class>::iterator it;
+  for (it = todds.begin(); it!=todds.end(); it++)
+    cout << *it << " ";
+  cout << endl;
 }
