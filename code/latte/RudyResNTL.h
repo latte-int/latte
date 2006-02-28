e665b8612c6d1fee8207f57e9a1df9b6676bc10b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-02-28 22:48:28 +0000

    (ResidueFunction_Single_Cone): Simplify arglist by moving Node_Controller into Single_Cone_Parameters. (Single_Cone_Parameters, Standard_Single_Cone_Parameters): Use subclassing and virtual method ConsumeCone to refactor the code.

--- code/latte/RudyResNTL.h
+++ code/latte/RudyResNTL.h
@@ -5,27 +5,44 @@
 #include "cone.h"
 #include "ramon.h"
 
-struct Single_Cone_Parameters
-{
-	listCone	*Cone;
+// Later we will reduce the slots in this class and use further
+// subclassing for the individual computation modes.  For instance,
+// Taylor_Expansion_Result is only used in the "dual" method.
+// -- mkoeppe
+
+class Single_Cone_Parameters {
+ public:
+	listCone	*Cone; // The master cone to be decomposed.
+	ZZ		*Total_Uni_Cones;
+	ZZ		*Current_Simplicial_Cones_Total;
+	ZZ		*Max_Simplicial_Cones_Total;
 	int		Number_of_Variables;
-	int		Degree_of_Taylor_Expansion;
 	unsigned int	Flags;
+	virtual int ConsumeCone(listCone *cone) = 0;
+};
+
+class Standard_Single_Cone_Parameters : public Single_Cone_Parameters {
+ public:
+	int		Degree_of_Taylor_Expansion;
 	
 	ZZ		*Taylor_Expansion_Result;
 	ZZ		*Random_Lambda;
 	ZZ		*Ten_Power;
 	ZZ		*Total_Lattice_Points;
-	ZZ		*Total_Uni_Cones;
-	ZZ		*Current_Simplicial_Cones_Total;
-	ZZ		*Max_Simplicial_Cones_Total;
+
+	Node_Controller *Controller;
+ public:
+	virtual int ConsumeCone(listCone *cone);
 };
+
 
 
 void ResidueFunction(listCone* cones, int numOfVars, int print_flag, int degree, int output_cone);
 
 // Returns -1 if a Dot Product is zero in the denominator, otherwise 1 if ok
-int	ResidueFunction_Single_Cone (Single_Cone_Parameters *Residue_Parameters, Node_Controller *Controller); 
+int
+ResidueFunction_Single_Cone (listCone *cones,
+			     Standard_Single_Cone_Parameters *Residue_Parameters); 
 
 #endif
 
