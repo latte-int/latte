2bdce66db73d19ae205090af4fdb2f33faa0f3af
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-02 01:05:49 +0000

    (transformArrayBigVectorToListVector2, createConeDecMatrix2): Unused, removed.

--- code/latte/dual.cpp
+++ code/latte/dual.cpp
@@ -120,40 +120,6 @@ using namespace std;
 //    return (dual->rest);
 //  }
 /* ----------------------------------------------------------------- */
-listVector* transformArrayBigVectorToListVector2(mat_ZZ A, int numOfVectors,
-						int numOfVars) {
-  int i;
-  vec_ZZ v;
-  listVector *L, *endL;
-
-  v=createVector(numOfVars);
-  L=createListVector(v);
-  endL=L;
-
-  for (i=0; i<numOfVectors; i++) {
-    v=A[i];
-    endL->rest = createListVector(v);
-    endL = endL->rest;
-  }
-
-  return (L->rest);
-}
-/* ----------------------------------------------------------------- */
-mat_ZZ createConeDecMatrix2(listVector *cones, int numOfRays, int numOfVars) {
-  int i;
-  mat_ZZ mat;
-  listVector *tmp;
-
-  mat.SetDims(numOfRays, numOfVars);
-
-  tmp=cones;
-  for (i=0; i<numOfRays; i++) {
-    mat[i]=copyVector(tmp->first,numOfVars);
-    tmp=tmp->rest;
-  }
-  removeListVector(cones);
-  return (mat);
-}
 /* ----------------------------------------------------------------- */
 
 listCone* dualizeCones(listCone *cones, int numOfVars) {
