953cf3e600efe816f5369db5d3cb65a3740e6ba7
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-02 23:34:15 +0000

    (GIVETIME, listRationalVector, inputFile): Unused, removed.

--- code/latte/myheader.h
+++ code/latte/myheader.h
@@ -13,18 +13,6 @@
 
 using namespace std;
 
-#ifdef SUN
-  #define GIVETIME times(&tms_buf);z=tms_buf.tms_utime;z=z/CLK_TCK;
-  #include <sys/param.h>
-  #include <sys/types.h>
-  #include <time.h>
-  #include <sys/times.h>
-#endif
-
-#ifdef LINUX
-  #define GIVETIME z=getcputime();
-#endif
-
 typedef ZZ Integer;
 
 struct listVector {
@@ -37,26 +25,15 @@ typedef struct rationalVector {
   vec_ZZ denominator;
 } rationalVector;
 
-typedef struct listRationalVector {
-  rationalVector* first;
-  struct listRationalVector *rest;
-} listRationalVector;
-
 typedef struct listCone {
   int coefficient;
-  ZZ determinant;
+  ZZ determinant;		// with sign
   rationalVector* vertex;
   listVector *rays;
   listVector *facets;
   listVector *latticePoints;
   struct listCone *rest;
 } listCone;
-
-typedef struct inputFile {
-  ifstream handle;
-  char char_buffer;
-  char token[256];
-} inputFile;
 
 #endif
 
