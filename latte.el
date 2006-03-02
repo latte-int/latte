daebe2e40bae2ada8ff918c25e9e6928d719642d
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-02 01:17:08 +0000

    *** empty log message ***

--- latte.el
+++ latte.el
@@ -1,7 +1,8 @@
 ;;; latte.el --- Emacs editing stuff for the LattE project
 
 (defconst ntl-types
-  '("ZZ" "vec_ZZ" "mat_ZZ"))
+  '("ZZ" "vec_ZZ" "mat_ZZ"
+    "RR" "vec_RR" "mat_RR"))
 
 (defconst gmp-types
   '("mpz_class" "mpq_class"))
@@ -9,9 +10,15 @@
 (defconst latte-types
   '("listCone" "listVector"
     "vector" "rationalVector"
+    "PtrCone" "Cone"
     "Integer"
     "mpq_vector" "mpz_vector"))
 
 (setq c++-font-lock-extra-types
       (cons (regexp-opt (append ntl-types latte-types gmp-types))
 	    c++-font-lock-extra-types))
+
+(if load-file-name
+    (let ((directory (file-name-directory load-file-name)))
+      (if (file-exists-p (concat directory "TAGS"))
+	  (visit-tags-table directory))))
