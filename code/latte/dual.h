5223b8dcbc672b8b7febc111b0874bacb3f4f0c1
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-03 23:26:25 +0000

    Add preprocessor magic.

--- code/latte/dual.h
+++ code/latte/dual.h
@@ -1,2 +1,11 @@
+// This is a -*- C++ -*- header file.
+
+#ifndef DUAL_H
+#define DUAL_H
+
+#include "myheader.h"
+
 listCone* dualizeCones(listCone*, int);
 listCone* dualizeBackCones(listCone*, int);
+
+#endif
