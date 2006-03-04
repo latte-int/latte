e1da51f36165d280c6937fee43a0c5801cbcb8c4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-04 21:24:15 +0000

    (listVector, rationalVector, listCone): Moved to cone.h, rational.h

--- code/latte/myheader.h
+++ code/latte/myheader.h
@@ -15,25 +15,5 @@ using namespace std;
 
 typedef ZZ Integer;
 
-struct listVector {
-  vec_ZZ first;
-  struct listVector *rest;
-};
-
-typedef struct rationalVector {
-  vec_ZZ enumerator;
-  vec_ZZ denominator;
-} rationalVector;
-
-typedef struct listCone {
-  int coefficient;
-  ZZ determinant;		// with sign
-  rationalVector* vertex;
-  listVector *rays;
-  listVector *facets;
-  listVector *latticePoints;
-  struct listCone *rest;
-} listCone;
-
 #endif
 
