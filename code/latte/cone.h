85c748be679b818b18ed656c8c8171532bcfbcad
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-04 21:20:41 +0000

    (listVector, listCone): Move here from myheader.h

--- code/latte/cone.h
+++ code/latte/cone.h
@@ -1,5 +1,27 @@
+/* This is a -*- C++ -*- header file. */
+
 #ifndef CONE_H
 #define CONE_H
+
+#include "rational.h"
+
+struct listVector {
+  vec_ZZ first;
+  struct listVector *rest;
+};
+
+typedef struct listCone {
+  int coefficient;
+  ZZ determinant;		// determinant of the matrix formed by
+				// the RAYS, with sign
+  rationalVector* vertex;
+  listVector *rays;
+  ZZ dual_determinant;		// determinant of the matrix formed by
+				// the FACETS, with sign
+  listVector *facets;
+  listVector *latticePoints;
+  struct listCone *rest;
+} listCone;
 
 listCone* createListCone();
 int lengthListCone(listCone*);
