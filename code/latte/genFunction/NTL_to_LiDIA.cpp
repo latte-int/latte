6e29d3a9ba922b9dcc551d3914dab1d293713ea8
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-03-30 00:41:33 +0000

    (convert_bigint_matrix_to_mat_ZZ): Don't convert via longs. (convert_vec_ZZ_to_bigint_array): Abort when numbers are too large.

--- code/latte/genFunction/NTL_to_LiDIA.cpp
+++ code/latte/genFunction/NTL_to_LiDIA.cpp
@@ -3,6 +3,7 @@
 #include <NTL/mat_ZZ.h>
 #include <LiDIA/bigint.h>
 #include <LiDIA/bigint_matrix.h>
+#include "latte_gmp.h"
 
 using namespace std;
 
@@ -57,28 +58,38 @@ convert_listVector_to_bigint_matrix(listVector *rays) {
 
 /* converts a NTL vec_ZZ data type to a bigint * array */
 bigint*
-convert_vec_ZZ_to_bigint_array(const vec_ZZ& ntl_v) {
-   bigint element;
-   /* allocate memory for the bigint array */
-   bigint *lidia_v = new bigint[ntl_v.length()];
-   for (int i = 0; i < ntl_v.length(); i++) {
-      /* copy from ZZ to bigint */
-      element.assign(to_long(ntl_v[i]));
-      lidia_v[i] = element;
-   }
-   return lidia_v;
+convert_vec_ZZ_to_bigint_array(const vec_ZZ& ntl_v)
+{
+  bigint element;
+  /* allocate memory for the bigint array */
+  bigint *lidia_v = new bigint[ntl_v.length()];
+  for (int i = 0; i < ntl_v.length(); i++) {
+    /* copy from ZZ to bigint */
+    if (abs(ntl_v[i]) > LONG_MAX) {
+      cerr << "copy_vec_ZZ_to_bigint_array: Hit implementation restriction" << endl;
+      abort();
+    }
+    element.assign(to_long(ntl_v[i]));
+    lidia_v[i] = element;
+  }
+  return lidia_v;
 }
 
 /* copies a NTL vec_ZZ data type to a bigint * array */
 void
-copy_vec_ZZ_to_bigint_array(bigint *lidia_v, const vec_ZZ& ntl_v) {
-   bigint vec_comp;
+copy_vec_ZZ_to_bigint_array(bigint *lidia_v, const vec_ZZ& ntl_v)
+{
+  bigint vec_comp;
 
-   for (int i = 0; i < ntl_v.length(); i++) {
-      /* copy from vector to bigint */
-      vec_comp.assign(to_long(ntl_v[i]));
-      lidia_v[i] = vec_comp;
-   }
+  for (int i = 0; i < ntl_v.length(); i++) {
+    /* copy from vector to bigint */
+    if (abs(ntl_v[i]) > LONG_MAX) {
+      cerr << "copy_vec_ZZ_to_bigint_array: Hit implementation restriction" << endl;
+      abort();
+    }
+    vec_comp.assign(to_long(ntl_v[i]));
+    lidia_v[i] = vec_comp;
+  }
 }
 
 /* LiDIA to NTL conversions */
@@ -89,13 +100,13 @@ convert_bigint_matrix_to_mat_ZZ(const bigint_matrix & lidia_m) {
    int rows = lidia_m.get_no_of_rows();
    int cols = lidia_m.get_no_of_columns();
    mat_ZZ ntl_m;
-   long elm;
 
    ntl_m.SetDims(rows, cols);
    for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-         (lidia_m.member(i,j)).longify(elm);
-         ntl_m[i][j] = elm;
+	//(lidia_m.member(i,j)).longify(elm);
+	mpz_class mpz(&lidia_m.member(i,j).bigint_rep());
+	ntl_m[i][j] = convert_mpz_to_ZZ(mpz);
       }
    }
    return (ntl_m);
