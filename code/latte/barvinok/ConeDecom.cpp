4c33af903d6f0da0b6216dc2b77544f733d29e59
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-11-27 16:57:08 +0000

    Use new infrastructure in triangulation.h

--- code/latte/barvinok/ConeDecom.cpp
+++ code/latte/barvinok/ConeDecom.cpp
@@ -32,7 +32,7 @@
 #include "../cone.h"
 #include "convert.h"
 #include "barvinok.h"
-#include "Triangulation.h"
+#include "triangulation/triangulate.h"
 
 /*
   The first step is to triangulate a cone into simplicial cones.
@@ -40,86 +40,21 @@
   simplicial cone into unimodular cones.
 */
 
-/* ----------------------------------------------------------------- */
-int barvinokDecomposition_Single(const mat_ZZ &Mat,
-				 const Vertex *vertex,
-				 Single_Cone_Parameters *Parameters) 
-{
-  /* m is the number of vectors and n is the number of dims. */
-
-  int m = Mat.NumRows();
-  int n = Parameters->Number_of_Variables;
-  assert(Mat.NumCols() == n);
-  
-  if((m == 0) || (n == 0)){
-    cerr << "The polytope is empty!" << endl;
-    exit(2);
-  }
-  int Face = 1, Faces = 10000;
-  char* s1 = "latte_dec";
-  list< int > List;
-  if(m != n){
-    Parameters->triangulate_time.start();
-    Face = Triangulation_Load_Save(Mat, m, n, s1, List, Parameters->File_Name, Parameters->Cone_Index, Parameters->Flags);
-    Parameters->triangulate_time.stop();
-  } /*Call triangulation fun.*/
-
-   /*
-     In this fun, a cone is decomposed into simplicial cones.
-   */
-  Faces = Face;
-  vector<mat_ZZ> B(Faces);
-
-  for(int i = 0; i < Faces; i++)
-    B[i].SetDims(n, n);
-
-  if(m != n){
-    long tmp = 0;
-    int counter = 0, index = 0;
-
-    while(!List.empty())
-     {
-       tmp = List.back();
-       List.pop_back();
-       B[index]((counter % n) + 1) = Mat(tmp);
-       counter++;
-       if((counter % n == 0))
-         index++;
-       
-     } 
-  }
-  if(m == n) B[0] = Mat;
-
-   /*
-     Call barvinok fun.  barvinok function decompose each
-     simplicial cone into unimodular cones.
-
-   */
-    for(int i = 0; i < Faces; i++){
-      if(IsZero(B[i]) != 1){
-        if ( barvinok_Single(B[i], Parameters, vertex) == -1)
-	{
-  		for(int i = 0; i < Faces; i++)
-			B[i].kill ();
-		return -1;
-	}
-      	}
-      }
-
-  for(int i = 0; i < Faces; i++)
-    B[i].kill();
-
-  return 1;
-}
-
 int
 barvinokDecomposition_Single (listCone *cone,
 			      Single_Cone_Parameters *Parameters)
 {
-  int numOfVars = Parameters->Number_of_Variables;
-  int numOfRays = lengthListVector(cone->rays);
-  mat_ZZ mat = createConeDecMatrix(cone,numOfRays,numOfVars);
-  return barvinokDecomposition_Single(mat, cone->vertex,
-				      Parameters);
+  int status = 1;
+  listCone *triang = triangulateCone(cone, Parameters->Number_of_Variables, Parameters);
+  listCone *t;
+  for (t = triang; t!=NULL; t=t->rest) {
+    int num_rays = lengthListVector(t->rays);
+    mat_ZZ B = createConeDecMatrix(t, num_rays, Parameters->Number_of_Variables);
+    if ((status = barvinok_Single(B, Parameters, t->vertex)) == -1)
+      goto BAILOUT;
+  }
+ BAILOUT:
+  freeListCone(triang);
+  return status;
 }
 
--- code/latte/barvinok/ConeDecom.h
+++ code/latte/barvinok/ConeDecom.h
@@ -27,28 +27,14 @@
 #include "barvinok.h"
 
 /* Do a signed decomposition, modulo lower-dimensional cones, of the
-   polyhedral cone spanned by the ROW VECTORS of B with apex at
-   CONE_VERTEX, until the determinants of all cones are at most
+   polyhedral CONE, until the determinants of all cones are at most
    PARAMETERS->max_determinant.  (When the cone is not simplicial, we
    triangulate it first.)
 
    Call PARAMETERS->ConsumeCone() for each of the small cones.
 */ 
 int
-barvinokDecomposition_Single (const mat_ZZ &B,
-			      const Vertex *Cone_Vertex,
-			      Single_Cone_Parameters *Parameters);
-
-/* Likewise, but the cone is given as a listCone (whose "rest" we
-   ignore). */ 
-int
 barvinokDecomposition_Single (listCone *cone,
 			      Single_Cone_Parameters *Parameters);
 
 #endif
-
-
-
-
-
-
