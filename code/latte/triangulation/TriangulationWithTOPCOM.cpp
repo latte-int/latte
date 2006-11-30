f897e0c5f865a2357f7d36895ad05fae50eb903f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-11-30 15:50:01 +0000

    Use TOPCOM binaries if library is not available.

--- code/latte/triangulation/TriangulationWithTOPCOM.cpp
+++ code/latte/triangulation/TriangulationWithTOPCOM.cpp
@@ -20,26 +20,45 @@
    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
 
+#include <list>
+#include <iostream>
+#include "config.h"
 #include "triangulation/TriangulationWithTOPCOM.h"
 #include "latte_gmp.h"
+#include "latte_system.h"
+#ifdef HAVE_TOPCOM_LIB
 #include <Matrix.hh>
 #include <PointConfiguration.hh>
 #include <PlacingTriang.hh>
 #include <Symmetry.hh>
 #include <SymmetricBFS.hh>
-#include <list>
+#endif
 
+using namespace std;
+
+static vector<listVector *>
+ray_array(listCone *cone)
+{
+  int num_rays = lengthListVector(cone->rays);
+  vector<listVector *> rays(num_rays);
+  int j;
+  listVector *ray;
+  for (j = 0, ray = cone->rays; ray!=NULL; j++, ray = ray->rest)
+    rays[j] = ray;
+  return rays;
+}
+
+
+//
+// Functions using the TOPCOM library.
+//
+#ifdef HAVE_TOPCOM
 static listCone *
 triangulation_to_cones(const SimplicialComplex &sc,
 		       listCone *cone)
 {
   // Copy rays into an array, so we can index them.
-  int num_rays = lengthListVector(cone->rays);
-  vector<listVector *> rays(num_rays);
-  size_type j;
-  listVector *ray;
-  for (j = 0, ray = cone->rays; ray!=NULL; j++, ray = ray->rest)
-    rays[j] = ray;
+  vector<listVector *> rays = ray_array(cone);
   // Iterate through simplices and collect them.
   listCone *cones = NULL;
   for (SimplicialComplex::const_iterator i = sc.begin(); i!=sc.end(); ++i) {
@@ -86,29 +105,116 @@ triangulate_cone_with_TOPCOM(listCone *cone, int numOfVars)
   std::cout << "Triangulation: " << triang << std::endl;
   return triangulation_to_cones(triang, cone);
 }
+#endif
+
+
+//
+// Functions using the TOPCOM binaries
+//
+
+#ifdef HAVE_TOPCOM_BIN
+
+static listCone *
+read_TOPCOM_triangulation(istream &in,
+			  listCone *cone)
+{
+  // Copy rays into an array, so we can index them.
+  vector<listVector *> rays = ray_array(cone);
+  // Read a triangulation in the TOPCOM format:
+  // T[1]:={{0,1,2,3,4,5,6,8},{0,1,2,3,4,5,7,8}};
+  char c;
+  // Find and consume left brace of triangulation.
+  do { in.get(c); } while (in.good() && c != '{');
+  listCone *cones = NULL;
+  do {
+    // Consume left brace of simplex.
+    in.get(c);
+    if (!(in.good() && c == '{')) break;
+    listCone *simp = createListCone();
+    simp->vertex = new Vertex(*cone->vertex);
+    while (in.good() && in.peek()!='}') {
+      int index;
+      in >> index;
+      simp->rays = new listVector(rays[index]->first, simp->rays);
+      // Skip the ',' of simplex.
+      if (in.peek()==',') in.get(c);
+    }
+    simp->rest = cones;
+    cones = simp;
+    if (!in.good()) break;
+    // Consume the '}' of simplex.
+    in.get(c);
+    // Expect ',' or '}' of triangulation.
+    in.get(c);
+    if (!in.good()) break;
+    if (c == '}') return cones;
+  } while (c == ',');
+  // Error situation:
+  freeListCone(cones);
+  return NULL;
+}
+
+static void
+write_TOPCOM_point_configuration(ostream &out,
+				 listCone *cone, int numOfVars)
+{
+  out << "[";
+  listVector *ray;
+  for (ray = cone->rays; ray!=NULL; ray=ray->rest) {
+    out << "[";
+    int i;
+    out << ray->first[0];
+    for (i = 1; i<numOfVars; i++)
+      out << ", " << ray->first[i];
+    out << "]";
+    if (ray->rest != NULL)
+      out << "," << endl;
+  }
+  out << "]" << endl;
+}
+
+#ifndef HAVE_TOPCOM_LIB
+// Alternative implementation if the TOPCOM library cannot be used.
+listCone *
+triangulate_cone_with_TOPCOM(listCone *cone, int numOfVars)
+{
+  string topcom_input_file_name = temporary_file_name("topcom_input");
+  string topcom_output_file_name = temporary_file_name("topcom_output");
+  {
+    ofstream topcom_input(topcom_input_file_name.c_str());
+    write_TOPCOM_point_configuration(topcom_input, cone, numOfVars);
+  }
+  system_with_error_check(TOPCOM_POINTS2PLACINGTRIANG " < " + topcom_input_file_name
+			  + " > " + topcom_output_file_name);
+  ifstream topcom_output(topcom_output_file_name.c_str());
+  listCone *triangulation = read_TOPCOM_triangulation(topcom_output, cone);
+  if (!triangulation) {
+    cerr << "Failed to read triangulation from TOPCOM output" << endl;
+    exit(1);
+  }
+  return triangulation;
+}
+#endif
 
 list<listCone *>
 all_triangulations_of_cone_with_TOPCOM(listCone *cone, int numOfVars)
 {
-  PointConfiguration points = cone_to_point_configuration(cone, numOfVars);
-  if ((points.no() < 2) || (points.rank() < 2)) {
-    std::cerr << "no of points and rank must be at least two." << std::endl;
-    exit(1);
+  string topcom_input_file_name = temporary_file_name("topcom_input");
+  string topcom_output_file_name = temporary_file_name("topcom_output");
+  {
+    ofstream topcom_input(topcom_input_file_name.c_str());
+    write_TOPCOM_point_configuration(topcom_input, cone, numOfVars);
   }
-  if (points.rank() > points.no()) {
-    std::cerr << "rank must not be larger than no of points." << std::endl;
-    exit(1);
+  system_with_error_check(TOPCOM_POINTS2TRIANGS " < " + topcom_input_file_name
+			  + " > " + topcom_output_file_name);
+  ifstream topcom_output(topcom_output_file_name.c_str());
+  list<listCone *> triangulations;
+  while (topcom_output.good()) {
+    listCone *triangulation = read_TOPCOM_triangulation(topcom_output, cone);
+    if (!triangulation) break;
+    triangulations.push_front(triangulation);
   }
-  Chirotope chiro(points, /*preprocess:*/ false);
-  size_type no(chiro.no());
-  size_type rank(chiro.rank());
-  SymmetryGroup symmetries(no);
-  SimplicialComplex seed = PlacingTriang(chiro);
-  const SymmetryGroup seed_symmetries(symmetries, seed);
-  
-  SymmetricBFS bfs(no, rank, points, chiro, 
-		   symmetries, seed, seed_symmetries, 
-		   /*output_triangs:*/ true, /*fine_only:*/ false);
-  
-  return list<listCone *>();
+  return triangulations;
 }
+
+#endif
