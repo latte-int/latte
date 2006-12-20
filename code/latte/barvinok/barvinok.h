6b7b15d9680ae278694a0db483768b5bb8e296af
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-12-20 10:20:18 +0000

    (DeloneTriangulationWithCddlib): New triangulation type.

--- code/latte/barvinok/barvinok.h
+++ code/latte/barvinok/barvinok.h
@@ -48,6 +48,7 @@ public:
   typedef enum {
     RegularTriangulationWithCdd,
     RegularTriangulationWithCddlib,
+    DeloneTriangulationWithCddlib,
     SubspaceAvoidingRecursiveTriangulation,
     SubspaceAvoidingBoundaryTriangulation,
     PlacingTriangulationWithTOPCOM
