492147b07484430b38f5562a3d607938ba65e687
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2006-12-21 13:32:21 +0000

    *** empty log message ***

--- code/latte/triangulation/BoundaryTriangulation.cpp
+++ code/latte/triangulation/BoundaryTriangulation.cpp
@@ -20,21 +20,32 @@
 
 #include <cassert>
 #include "triangulation/BoundaryTriangulation.h"
+#include "triangulation/RegularTriangulationWithCddlib.h"
 #include "latte_cddlib.h"
+#include "latte_random.h"
 #include "print.h"
+#include "dual.h"
 
 using namespace std;
+
+static bool
+rays_ok(listCone *cone, int numOfVars)
+{
+  listVector *ray;
+  for (ray = cone->rays; ray!=NULL; ray=ray->rest) {
+    if (ray->first[numOfVars - 1] == 0)
+      return false;
+  }
+  return true;
+}
 
 static void
 check_rays(listCone *cone, int numOfVars)
 {
-  listVector *ray;
-  for (ray = cone->rays; ray!=NULL; ray=ray->rest) {
-    if (ray->first[numOfVars - 1] == 0) {
-      cerr << "The following dualized-back cone has bad rays." << endl;
-      printListCone(cone, numOfVars);
-      abort();
-    }
+  if (!rays_ok(cone, numOfVars)) {
+    cerr << "The following dualized-back cone has bad rays." << endl;
+    printCone(cone, numOfVars);
+    abort();
   }
 }
 
@@ -42,6 +53,8 @@ listCone *
 boundary_triangulation_of_cone_with_subspace_avoiding_facets
 (listCone *cone, BarvinokParameters *Parameters)
 {
+  listCone *boundary_triangulation = NULL;
+  vector<listVector *> rays = ray_array(cone);
   dd_PolyhedraPtr poly
     = cone_to_cddlib_polyhedron(cone, Parameters->Number_of_Variables);
   dd_MatrixPtr inequalities = dd_CopyInequalities(poly);
@@ -61,7 +74,63 @@ boundary_triangulation_of_cone_with_subspace_avoiding_facets
 	cout << j + 1 << " ";
       }
     cout << "(cardinality " << set_card(incidence->set[i]) << ")" << endl;
+    /* Compute a triangulation of that facet. */
+    listCone *facet_cone
+      = cone_from_ray_set(rays, incidence->set[i], cone->vertex);
+    listCone *facet_triangulation
+      = triangulate_cone_with_cddlib(facet_cone, Parameters,
+				     delone_height, NULL,
+				     Parameters->Number_of_Variables - 1);
+    cout << "Triangulation of facet cone: " << lengthListCone(facet_triangulation)
+	 << " simplicial cones." << endl;
+    boundary_triangulation
+      = appendListCones(facet_triangulation, boundary_triangulation);
   }
-  cerr << "Code unfinished." << endl;
-  abort();
+  /* Complete the cones with an interior ray. */
+  vec_ZZ interior_ray_vector;
+  interior_ray_vector.SetLength(Parameters->Number_of_Variables);
+  for (i = 0; i<rays.size(); i++) {
+    interior_ray_vector += rays[i]->first;
+  }
+  listCone *simplicial_cone;
+  for (simplicial_cone = boundary_triangulation;
+       simplicial_cone!=NULL;
+       simplicial_cone = simplicial_cone->rest) {
+    simplicial_cone->rays
+      = appendVectorToListVector(interior_ray_vector, simplicial_cone->rays);
+  }
+  bool triangulation_ok = true;
+  do {
+    /* Pick a random interior ray vector. */
+    int j;
+    for (j = 0; j<Parameters->Number_of_Variables; j++) {
+      interior_ray_vector[j] = uniform_random_number(1, 100);
+    }
+    cout << "Interior ray vector: " << interior_ray_vector << endl;
+    /* Replace it in all cones and clear all facets. */
+    for (simplicial_cone = boundary_triangulation;
+	 simplicial_cone!=NULL;
+	 simplicial_cone = simplicial_cone->rest) {
+      simplicial_cone->rays->first = interior_ray_vector;
+      if (simplicial_cone->facets != NULL) {
+	freeListVector(simplicial_cone->facets);
+	simplicial_cone->facets = NULL;
+      }
+    }
+    /* Compute and check the facets. */
+    boundary_triangulation
+      = dualizeBackCones(boundary_triangulation, Parameters->Number_of_Variables);
+    for (simplicial_cone = boundary_triangulation;
+	 simplicial_cone!=NULL;
+	 simplicial_cone = simplicial_cone->rest) {
+      if (!rays_ok(simplicial_cone, Parameters->Number_of_Variables)) {
+	triangulation_ok = false;
+	break;
+      }
+    }
+    boundary_triangulation
+      = dualizeBackCones(boundary_triangulation, Parameters->Number_of_Variables);
+  } while (!triangulation_ok);
+
+  return boundary_triangulation;
 }
