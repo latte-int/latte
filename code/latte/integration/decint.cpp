33fe65886292df0aae1db00120d3593f1569b5fc
Wu Jianqiu <wxwujianqiu@gmail.com>
2010-01-13 03:09:45 +0000

    new integration, type an option for the mode of integration: polynoimal or linear

--- code/latte/integration/decint.cpp
+++ code/latte/integration/decint.cpp
@@ -11,32 +11,39 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) { cout << "Usage: ./integrate fileIn" << endl; return 1; }
+	if (argc < 3) { cout << "Usage: ./integrate fileIn polynomial (or linear)" << endl; return 1; };
 	string line,line2;
 	polynomial myPoly;
 	linearPoly lForm;
 	ifstream myStream (argv[1]);
-//	ofstream outStream(argv[2]);
 	if (!myStream.is_open()) { cout << "Error opening file " << argv[1] << ", please make sure it is spelled correctly." << endl; return 1; };
+	if (!strcmp("polynomial",argv[2])) 
+	{
 		myStream >> line;
 		loadPolynomial(myPoly, line);
 		lForm.termCount = 0;
-		lForm.varCount = myPoly.varCount;
-		
+		lForm.varCount = myPoly.varCount;		
 		cout << "Decomposing";
 		for (int i = 0; i < myPoly.termCount; i++)
 		{
 			cout << ".";
 			decompose(myPoly, lForm, i);
-		}
+		};
 		cout << endl;
  		cout << "The polynomial is:" <<line<<endl;
 		cout << "Integrating by decomposition" << endl;
 		myStream >> line2;	//line2 is the simplex for example, [[0,0],[1,2],[2,3]]
-		print_Integrate(myPoly.varCount,lForm,line2);
+		integrateFlatVector(myPoly.varCount, lForm, line2);
 		destroyForm(lForm);
 		destroyPolynomial(myPoly);
+	}
+	else if (!strcmp("linear",argv[2])) 
+	{
+		myStream >> line;
+		myStream >> line2;
+		cout << "The linear form is:"<<line<<endl;
+		integrateList(line, line2);
+	};
 	myStream.close();
-//	outStream.close();
 	return 0; 
 }
--- code/latte/integration/newIntegration.h
+++ code/latte/integration/newIntegration.h
@@ -10,14 +10,10 @@ using namespace std;
 
 NTL_CLIENT
 
-void print_Integrate(int d, const linearPoly &lForm , string &line)
+void convertToSimplex(int d, vec_vec_ZZ &s, ZZ &v, string line)
 {
-  ZZ v,de,sum,lcm,total,g,a,b,counter,m,tem,coe;
-	int i,j,index,k,t;
-	vec_ZZ inner_Pro,sum_Nu,sum_De,l;
-	vec_vec_ZZ s;
+	int index,i,t,j;
 	string temp,subtemp;
-	//convert the string into a vec_vec_ZZ
  	s.SetLength(d+1);
 	index=1;
 	for (i=0;i<=d;i++)
@@ -36,17 +32,105 @@ void print_Integrate(int d, const linearPoly &lForm , string &line)
 		s[i][d-1]=to_ZZ(subtemp.c_str());
 		index=line.find("]",index)+2;
 	};
-	inner_Pro.SetLength(d+1);
-	sum_Nu.SetLength(d+1);
-	sum_De.SetLength(d+1);
-	l.SetLength(d);
-	lcm=1; //least common multiple
-	total=0; //total numerator
 	v=1;
 	mat_ZZ matt;	
 	matt.SetDims(d,d);
 	for (i=1;i<=d;i++) matt[i-1]=s[i]-s[0];
 	v=determinant(matt);if (v<0) v=-v;
+};
+
+void update(ZZ &a, ZZ &b, vec_ZZ l, vec_vec_ZZ s,ZZ m, ZZ coe, ZZ v, int d, ZZ de)
+{
+	ZZ sum,lcm,total,g,tem;
+	int i,j;
+	vec_ZZ inner_Pro,sum_Nu,sum_De;
+	inner_Pro.SetLength(d+1);
+	sum_Nu.SetLength(d+1);
+	sum_De.SetLength(d+1);
+	total=0;
+	lcm=1;
+	for (i=0;i<=d;i++)
+	{
+		sum=0; for (j=0;j<d;j++) {sum=sum+l[j]*s[i][j];};
+		inner_Pro[i]=sum;
+	};//stores inner product for use
+	for (i=0;i<=d;i++)
+	{
+		sum_Nu[i]=1;for (j=0;j<m+d;j++) sum_Nu[i]=sum_Nu[i]*inner_Pro[i];
+		sum_De[i]=1;for (j=0;j<=d;j++) if (i!=j) sum_De[i]=sum_De[i]*(inner_Pro[i]-inner_Pro[j]);
+		if (sum_De[i]==0) {cout<<l<<"is not regular!"; return ;}; //irregular
+		lcm=lcm*sum_De[i]/(GCD(lcm,sum_De[i]));
+	};
+	for (i=0;i<=d;i++)
+	{
+		total+=sum_Nu[i]*(lcm/sum_De[i]);
+	};
+	lcm=lcm*de;
+	total=total*v*coe;
+	if (a==0) {a=total;b=lcm;}
+	else {tem=b*lcm/GCD(b,lcm);a=a*tem/b+total*tem/lcm;b=tem;};	
+	g=GCD(a,b);
+	a=a/g;
+	b=b/g;
+}
+
+void integrateList(string line, string line2)
+{
+	ZZ v,de,a,b,counter,m,tem,coe;
+	int t,tt,i,j,index,k,d;
+	vec_ZZ l;
+	vec_vec_ZZ s;
+	string temp,subtemp;
+	t=2;d=1;
+	t=line.find("[",t)+1;t=line.find("[",t)+1;
+	temp=line.substr(t,line.find("]",t)-t);
+	for (i=0;i<temp.length();i++) d+=(temp.at(i)==',');
+	convertToSimplex(d,s,v,line2);//convert the string into a vec_vec_ZZ
+	l.SetLength(d);
+	index=2;
+	a=0;
+	b=0;
+	while (index<line.length())
+	{
+		t=line.find("]]]",index);
+		temp=line.substr(index,t-index);
+		subtemp=temp.substr(0,temp.find(","));
+		t=temp.find("[",0)+1;
+		coe=to_ZZ(subtemp.c_str());
+		subtemp=temp.substr(t,temp.find(",",t)-t);
+		m=to_ZZ(subtemp.c_str());
+		de=1;
+		for (i=1;i<=d;i++)
+		{
+			de=de*(m+i);
+		};
+		t=temp.find("[",t)+1;
+		temp=temp.substr(t,temp.length()-t+1);
+		t=0;
+		for (i=0;i<d-1;i++)
+		{
+			tt=temp.find(",",t);
+			subtemp=temp.substr(t,tt-t);
+			l[i]=to_ZZ(subtemp.c_str());
+			t=tt+1;
+		}
+		subtemp=temp.substr(t,temp.length()-t+1);
+		l[d-1]=to_ZZ(subtemp.c_str());
+		update(a,b,l,s,m,coe,v,d,de);
+		index=line.find("]]]",index)+5;
+	};
+	if (b<0) {b=-b;a=-a;};
+	cout<<"The desired integral is equal to:"<<a<<"/"<<b<<endl;
+};
+
+void integrateFlatVector(int d, const linearPoly &lForm , string line)
+{
+  ZZ v,a,b,de,counter,m,tem,coe;
+	int i,j,index,k;
+	vec_ZZ l;
+	vec_vec_ZZ s;
+	convertToSimplex(d,s,v,line);//convert the string into a vec_vec_ZZ
+	l.SetLength(d);
 	lBlock* temForm=lForm.lHead;
 	cBlock* temCoef=lForm.cHead;
 	k=-1;
@@ -61,35 +145,11 @@ void print_Integrate(int d, const linearPoly &lForm , string &line)
 		m=temForm->degree[k];
 		coe=temCoef->data[k];
 		de=1;
-		total=0;
-		lcm=1;
 		for (i=1;i<=d+m;i++)
 		{
 			de=de*i;
 		};
-		for (i=0;i<=d;i++)
-		{
-			sum=0; for (j=0;j<d;j++) {sum=sum+l[j]*s[i][j];};
-			inner_Pro[i]=sum;
-		};//stores inner product for use
-		for (i=0;i<=d;i++)
-		{
-			sum_Nu[i]=1;for (j=0;j<m+d;j++) sum_Nu[i]=sum_Nu[i]*inner_Pro[i];
-			sum_De[i]=1;for (j=0;j<=d;j++) if (i!=j) sum_De[i]=sum_De[i]*(inner_Pro[i]-inner_Pro[j]);
-			if (sum_De[i]==0) {cout<<l<<"is not regular!"; return ;}; //irregular
-			lcm=lcm*sum_De[i]/(GCD(lcm,sum_De[i]));
-		};
-		for (i=0;i<=d;i++)
-		{
-			total+=sum_Nu[i]*(lcm/sum_De[i]);
-		};
-		lcm=lcm*de;
-		total=total*v*coe;
-		if (a==0) {a=total;b=lcm;}
-		else {tem=b*lcm/GCD(b,lcm);a=a*tem/b+total*tem/lcm;b=tem;};	
-		g=GCD(a,b);
-		a=a/g;
-		b=b/g;
+		update(a,b,l,s,m,coe,v,d,de);
 	}
 	if (b<0) {b=-b;a=-a;};
 	cout<<"The desired integral is equal to:"<<a<<"/"<<b<<endl;
