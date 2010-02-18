7a6518394156277353adc532306c62a5c0583a90
Wu Jianqiu <wxwujianqiu@gmail.com>
2010-02-18 22:43:46 +0000

    deleted debuging output This line, and those below, will be ignored--
    M    integration/newIntegration.cpp

--- code/latte/integration/newIntegration.cpp
+++ code/latte/integration/newIntegration.cpp
@@ -83,15 +83,15 @@ void update(ZZ &a, ZZ &b, vec_ZZ l, simplexZZ mySimplex,int m, ZZ coe, ZZ de)
 		if ((sum_Nu[i]<0)&&(sum_De[i]<0)) {sum_Nu[i]=-sum_Nu[i];sum_De[i]=-sum_De[i];};
 		if (sum_De[i]==0)
 		{
-			cout<<l<<" is not regular at "<<inner_Pro[i]<<". Residue:";
+			//cout<<l<<" is not regular at "<<inner_Pro[i]<<". Residue:";
 			vec_ZZ ProDiff;
 			ProDiff.SetLength(mySimplex.d+1);
 			for (j=0;j<=mySimplex.d;j++) ProDiff[j]=inner_Pro[i]-inner_Pro[j];
 			//cout<<"The inner product difference is"<<ProDiff<<" inner product at vertex is "<<inner_Pro[i]<<". Now compute residue:"<<endl;
 			computeResidue(mySimplex.d,m,ProDiff,inner_Pro[i],sum_Nu[i],sum_De[i]);
 		} //irregular
-		else cout<<l<<" is regular at "<<inner_Pro[i]<<". Sum:";
-		cout<<sum_Nu[i]<<"/"<<sum_De[i]<<endl;
+		//else cout<<l<<" is regular at "<<inner_Pro[i]<<". Sum:";
+		//cout<<sum_Nu[i]<<"/"<<sum_De[i]<<endl;
 		if (sum_De[i]!=0) {lcm=lcm*sum_De[i]/(GCD(lcm,sum_De[i]));};
 	};
 	for (i=0;i<=mySimplex.d;i++)
