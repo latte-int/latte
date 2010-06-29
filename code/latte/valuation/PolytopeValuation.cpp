045fefc85439f385f2a2e2c6e91280fa9e80c4d6
Brandon Dutra <bedutra@ucdavis.edu>
2010-06-29 20:04:46 +0000

    Added computing volume by trangulating the polytope for polytopes with INTEGER vertices only. This will need to be updated later.

--- code/latte/valuation/PolytopeValuation.cpp
+++ code/latte/valuation/PolytopeValuation.cpp
@@ -7,11 +7,14 @@
 
 #include "PolytopeValuation.h"
 
-
 using namespace std;
 
-
-PolytopeValuation::PolytopeValuation()
+/**
+ * Does not keep a local copy of the polyhedron, only a pointer.
+ * Polyhedron *poly contains the list of vertex-ray pairs.
+ */
+PolytopeValuation::PolytopeValuation(Polyhedron *p, BarvinokParameters *bp) :
+	poly(p), parameters(bp), polytopeAsOneCone(NULL), triangulatedPoly(NULL)
 {
 	// TODO Auto-generated constructor stub
 
@@ -19,101 +22,141 @@ PolytopeValuation::PolytopeValuation()
 
 PolytopeValuation::~PolytopeValuation()
 {
-	// TODO Auto-generated destructor stub
+	//don't free parameters or poly, because we did not make them!
+
+	if (polytopeAsOneCone) freeListCone(polytopeAsOneCone);
+	if (triangulatedPoly) freeListCone(triangulatedPoly);
 }
 
-
-
-
-void PolytopeValuation::findVolume(listCone *cones, int numOfVars, unsigned int Flags,
-	       char *File_Name, int max_determinant,
-	       bool dualize,
-	       BarvinokParameters::DecompositionType decomposition)
+void PolytopeValuation::convertToOneCone()
 {
-	  Collecting_Single_Cone_Parameters parameters;
-	  parameters.Flags = Flags;
-	  parameters.Number_of_Variables = numOfVars;
-	  parameters.max_determinant = max_determinant;
-	  parameters.File_Name = File_Name;
-	  parameters.decomposition = decomposition;
-
-	  listCone * allDecompCones = decomposeCones(cones, dualize, parameters);
-
-	  printListConeToFile("cones.decomposed", allDecompCones, numOfVars);
-
-	  cout << "findVolumn():: total of simple cones:" << lengthListCone(allDecompCones) << endl;
-
-	  ZZ sum;
-
-	  for(listCone * current = allDecompCones; current; current = current->rest)
-		  sum += current->coefficient * current->dual_determinant;
-	  cout << "PolytopeValuation::findVolumn:: total = " << sum << endl;
-}//findVolumn
-
-void PolytopeValuation::findVolumeSingle(listCone *cones, int numOfVars,
-		unsigned int flags, char *File_Name, int max_determinant, bool dualize,
-		BarvinokParameters::DecompositionType decomposition)
-{
-	Standard_Single_Cone_Parameters *Barvinok_Parameters =
-			new Standard_Single_Cone_Parameters;
-
-	Barvinok_Parameters->Flags = flags;
-	Barvinok_Parameters->Number_of_Variables = numOfVars;
-	Barvinok_Parameters->max_determinant = max_determinant;
-	Barvinok_Parameters->File_Name = File_Name;
-	Barvinok_Parameters->decomposition = decomposition;
-
-	decomposeAndReturnCones(cones, dualize, *Barvinok_Parameters);
-
-	delete Barvinok_Parameters;
-
-	printListConeToFile("cones.decomposedSingle", cones, numOfVars);
-
-}
-
-
-
-listCone * PolytopeValuation::decomposeAndReturnCones(listCone *cones, bool dualize,
-			   Standard_Single_Cone_Parameters &param)
-{
-	int numOfAllCones;
-	mat_ZZ mat;
-
-	if (dualize)
+	if ( ! poly )
 	{
-		dualizeCones(cones, param.Number_of_Variables, &param);
+		cout << "Polyhedron* is not defined" << endl;
+		exit(1);
+	}//error.
+
+	listCone * oneCone = new listCone();
+	oneCone->coefficient = 1;
+	oneCone->determinant = 0;
+	oneCone->subspace_generators = NULL;
+	oneCone->dual_determinant = 0;
+	oneCone->facets = NULL;
+	oneCone->equalities = NULL;
+	oneCone->latticePoints = NULL;
+	oneCone->rest = NULL;
+
+	//set to zero vector of numofvars + 1 size.
+	oneCone->vertex = new Vertex();
+	oneCone->vertex->vertex = new rationalVector(poly->numOfVars + 1);
+
+	oneCone->rays = new listVector;
+	oneCone->rays->rest = 0;
+
+	//now add the vertex to the rays list with a leading 1: (1, old poly cone vertex).
+	//The first entry in masterList should be ignored because masterList->first = masterList->rest->first.
+	listVector * masterList = new listVector;
+
+
+	//FIXME: rays are integer, but vertex are rational. Will need to chagne this.
+	//cout << "BUILDING THE RAYS" << endl;
+	for (listCone * currentCone = poly->cones; currentCone; currentCone
+			= currentCone->rest)
+	{
+		vec_ZZ buildRay; //buildRay = [1, old-vertex]
+		ZZ nume, denom;
+		buildRay.SetLength(poly->numOfVars + 1);
+		buildRay[0] = 1;
+
+
+		for (int i = 0; i < poly->numOfVars; ++i)
+		{
+
+			currentCone->vertex->vertex->getEntry(i, nume, denom); //get vertex[i] = nume/denome.
+			if (denom != 1 || denom == 0)
+			{
+				cerr
+						<< "Converting from rational vector to vector failed because denom. = "
+						<< denom << endl;
+				exit(1);
+			}//if error.
+
+			buildRay[i + 1] = nume;
+		}//for i
+
+		//cout << buildRay << endl;
+
+		masterList->first = buildRay;
+		masterList = appendVectorToListVector(buildRay, masterList);
+	}//for currentCone
+
+	//	cout << "END  BUILDING THE RAYS" << endl;
+
+	oneCone->rest = 0;
+	oneCone->rays = masterList->rest; //ignore masterList->first, so just return the rest and NOT masterList.
+
+	if ( polytopeAsOneCone) delete polytopeAsOneCone;
+	polytopeAsOneCone = oneCone; //finally, save the result.
+}//convertToOneCone
+
+
+
+
+RR PolytopeValuation::findDetermiantForVolume(const listCone * oneSimplex) const
+{
+	int i, numOfRays;
+	mat_ZZ mat;
+	listVector *tmp;
+
+	numOfRays = lengthListVector(oneSimplex->rays);
+
+	mat.SetDims(numOfRays - 1, poly->numOfVars);
+
+	tmp = oneSimplex->rays;
+	vec_ZZ startingRay = tmp->first;
+	for (i = 1; i < numOfRays; i++)
+	{
+		tmp = tmp->rest;
+		vec_ZZ endingRay = tmp->first - startingRay; //FIXME: must convert to vect_RR by dividing by leading element (to get the form [1 old vertex])
+
+		for(int j = 1; j < poly->numOfVars + 1 ; ++j)
+			mat[i - 1][j - 1] = endingRay[j];
 	}
 
+	return to_RR(determinant(mat))/to_RR(factorial(numOfRays -1));
+}//findDetermiantForVolume
 
-	numOfAllCones = lengthListCone(cones);
-	cerr << numOfAllCones << " cones total to be processed";
+RR PolytopeValuation::findVolume()
+{
+	RR sum;
+	sum = 0;
 
-	//Set Ten_Power to 100 billion
-	// FIXME: What is magic about this number? --mkoeppe, Sat Mar  4 21:21:45 PST 2006
-	param.Ten_Power = 1;
-	for (int i = 0; i < 12; i++)
-		param.Ten_Power *= 10;
+	convertToOneCone();
+	triangulatePolytopeCone();
 
+	for (listCone * oneSimplex = triangulatedPoly; oneSimplex; oneSimplex = oneSimplex->rest)
+	{
+		sum += abs(findDetermiantForVolume(oneSimplex));
+	}
 
-	param.Taylor_Expansion_Result = new ZZ[1 + 1];
+	return sum;
 
-	param.Degree_of_Taylor_Expansion = 1;
-	param.Controller = new Node_Controller(param.Number_of_Variables + 1,
-			1);
-
-	cerr << "Number of cones: " << numOfAllCones << endl;
-
-	barvinokDecomposition_List(cones, param);
-
-	cerr << endl << "Total Unimodular Cones: " << param.Total_Uni_Cones << endl;
+}//findVolume
 
 
 
+ZZ  PolytopeValuation::factorial(const int n)
+{
+	ZZ product;
+	product = 1;
+	for(int i = n; i > 1; --i)
+		product *= i;
+	return product;
+}//factorial
 
-}//decomposeAndReturnCones
 
-
-
-
-
-
+void PolytopeValuation::triangulatePolytopeCone()
+{
+	parameters->Number_of_Variables = poly->numOfVars + 1;
+	triangulatedPoly = triangulateCone(polytopeAsOneCone, poly->numOfVars + 1, parameters);
+}//triangulateCone()
--- code/latte/valuation/PolytopeValuation.h
+++ code/latte/valuation/PolytopeValuation.h
@@ -8,68 +8,49 @@
 #ifndef POLYTOPEVALUATION_H_
 #define POLYTOPEVALUATION_H_
 
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-#include <string.h>
-#include <stdio.h>
-
-#include "config.h"
-#include "barvinok/dec.h"
 #include "barvinok/barvinok.h"
-#include "barvinok/Triangulation.h"
-#include "vertices/cdd.h"
-#include "genFunction/maple.h"
-#include "genFunction/piped.h"
-#include "cone.h"
-#include "dual.h"
-#include "RudyResNTL.h"
-#include "Residue.h"
-#include "Grobner.h"
-//  #include "jesus.h"
-#include "preprocess.h"
-#include "print.h"
-#include "ramon.h"
-#include "rational.h"
-#include "timing.h"
-#include "flags.h"
-//#include "testing.h"
-#include "IntegralHull.h"
-#include "ReadingFile.h"
-#include "binarySearchIP.h"
-#include "CheckEmpty.h"
-#include "ProjectUp.h"
-
-#include "banner.h"
+#include "ReadPolyhedron.h"
+#include "triangulation/triangulate.h"
 #include "convert.h"
-#include "latte_system.h"
-
+#include "print.h"
 #include "gnulib/progname.h"
+#include "barvinok/dec.h"
+#include "valuation/PolytopeValuation.h"
+#include <NTL/vec_ZZ.h>
+#include "rational.h"
+#include "cone.h"
 
-
+using namespace std;
 
 
 
 
 class PolytopeValuation
 {
+
+	Polyhedron *poly;				//lists of vertex-ray pairs.
+	BarvinokParameters *parameters; //Barvinok Parameters.
+	listCone * polytopeAsOneCone;	//From poly, create one code with vertex=[0,0...0], rays={[1, v] | v is a vertex of the polytope}
+	listCone * triangulatedPoly;	//The triangulation of polytopeAsOneCone.
+
+
+
 public:
-	PolytopeValuation();
+	PolytopeValuation(Polyhedron *p, BarvinokParameters *bp);
 	virtual ~PolytopeValuation();
 
 
 	// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 
-
-	void findVolume(listCone *cones, int numOfVars, unsigned int Flags,
-		       char *File_Name, int max_determinant,
-		       bool dualize,
-		       BarvinokParameters::DecompositionType decomposition);
-
-	void findVolumeSingle(listCone *cones, int numOfVars,
-			unsigned int flags, char *File_Name, int max_determinant, bool dualize,
-			BarvinokParameters::DecompositionType decomposition);
-
-	listCone * decomposeAndReturnCones(listCone *cones, bool dualize,
-				   Standard_Single_Cone_Parameters &param);
+	void convertToOneCone(); //convert from poly to polytopeAsOneCone
+	RR findDetermiantForVolume(const listCone * oneSimplex) const;
+	RR findVolume();		 //finds the volume of the Polyhedron.
+	ZZ static factorial(const int n);
+	void triangulatePolytopeCone();  //convert polytopeAsOneCone to triangulatedPoly
 
 };
 
