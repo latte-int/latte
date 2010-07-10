6db7b6541c388fa2384f36e7af80c56b94473bb7
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2010-07-10 05:00:57 +0000

    testIntegration.sh and testMultiply.sh are now generated files.

--- code/latte/integration/testIntegration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cat /dev/null > integration/status.txt
-
-maple -q integration/decomposeTest.mpl #| tee 2>&1 | tee -a integration/status.txt | egrep "total errors" | tr -d '"' | awk {'exit $1'}
