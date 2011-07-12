09cd554ab3455d3f4e79c9f9ae4ce3c80ed28948
Brandon Dutra <bedutra@ucdavis.edu>
2011-07-12 00:00:50 +0000

    added the unfinished stokes files.

---
+++ code/latte/valuation/RecursivePolytopeValuation.h
@@ -0,0 +1,31 @@
+/*
+ * RecursivePolytopeValuation.h
+ *
+ *  Created on: May 22, 2011
+ *      Author: bedutra
+ */
+
+#ifndef RECURSIVEPOLYTOPEVALUATION_H_
+#define RECURSIVEPOLYTOPEVALUATION_H_
+
+#include "PolytopeValuation.h"
+
+class RecursivePolytopeValuation {
+public:
+	RecursivePolytopeValuation();
+	virtual ~RecursivePolytopeValuation();
+
+
+	RationalNTL findVolume(ReadPolyhedronDataRecursive & readPolyhedron, BarvinokParameters * parm);
+	RationalNTL findVolume_recursive(ReadPolyhedronDataRecursive & readPolyhedron, BarvinokParameters * parm, int power, vec_ZZ &l);
+
+	void setMaxRecursiveLevel(int);
+	void setMinDimension(int); //at what dimension is the integration "easy" and stokes need not  be used?
+private:
+	int maxRecursiveLevel;
+	int recursiveLevel;
+	int minAffineDimension;
+
+};
+
+#endif /* RECURSIVEPOLYTOPEVALUATION_H_ */
