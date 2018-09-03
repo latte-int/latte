c79e62827067f56eac24d0056c80a7dfe0860708
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-09-03 13:27:22 -0700

    .travis.yml: Use shared distro build for shared build

--- .travis-print-logs.sh
+++ .travis-print-logs.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
-for a in `find . -name "testingSLandTopEhrhart" -prune -o -name "*.log" -print -o -name "log" -print`; do
+# First test-suite summary logs
+LOGS=`find . -name "testingSLandTopEhrhart" -prune -o -name "config.log" -prune -o -name "*.log" -print`
+# Then detailed results logs
+LOGS="$LOGS `find . -name "log" -print`"
+# Finally the long config.log
+LOGS="$LOGS `find . -name config.log`"
+for a in $LOGS; do
     section=`echo $a | sed 's/[^[:alnum:]]/_/g;'`
     echo "###########################################################################"
     echo "travis_fold:start:${section}"
