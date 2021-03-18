cdf644a4076e371d52f2dfebd408f14cca4b9da6
Jan Engelhardt <jengelh@inai.de>
2021-03-17 17:38:20 -0700

    Simplify autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 #gnulib-tool --m4-base=m4/gnulib --update
-aclocal -I m4 -I m4/gnulib
-autoheader
-autoconf
-libtoolize --force || glibtoolize --force
-automake --add-missing
+autoreconf -fi
